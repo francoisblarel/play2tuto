fe0d1e26e6d8f7312679d4dce2fd918da66c178a
François Blarel <francois.blarel@gmail.com>
2013-03-29 17:54:45 +0100

    Update Application.scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,13 +6,6 @@ import play.api.mvc._
 
 object Application extends Controller with SecurityTrait{
 
-  private val loginForm : Form[User] = Form(
-    mapping(
-        "login" -> nonEmptyText,
-        "password" -> nonEmptyText
-    )(User.apply)(User.unapply)
-  )
-
   def index = isAuthenticated{ username => implicit request =>
     Logger.info("hello "+username)
     Redirect(routes.Products.list())
